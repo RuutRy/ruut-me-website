556e9dd84e94d2aa09994023be3d39dbcbcd4fff
ekosfin <elias.samuli@gmail.com>
2023-10-15 09:41:34 +0300

    Updated terraform for testing

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,9 @@
 
 locals {
-  tags                         = { azd-env-name : var.environment_name }
-  sha                          = base64encode(sha256("${var.environment_name}${var.location}${data.azurerm_client_config.current.subscription_id}"))
-  resource_token               = substr(replace(lower(local.sha), "[^A-Za-z0-9_]", ""), 0, 13)
-  cosmos_connection_string_key = "AZURE-COSMOS-CONNECTION-STRING"
+  tags = { azd-env-name : var.environment_name }
+  # sha                          = base64encode(sha256("${var.environment_name}${var.location}${data.azurerm_client_config.current.subscription_id}"))
+  # resource_token               = substr(replace(lower(local.sha), "[^A-Za-z0-9_]", ""), 0, 13)
+  # cosmos_connection_string_key = "AZURE-COSMOS-CONNECTION-STRING"
 }
 # ------------------------------------------------------------------------------------------------------
 # Deploy resource Group
