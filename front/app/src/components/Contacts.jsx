f44f255911c978b5c74d413c130fb792205d1fe7
ekosfin <elias.samuli@gmail.com>
2024-03-11 16:33:51 +0200

    Updated emails for board

--- front/app/src/components/Contacts.jsx
+++ front/app/src/components/Contacts.jsx
@@ -9,43 +9,43 @@ class Contacts extends Component {
           <h3>Puheenjohtaja</h3>
           <p>Miko Mattila</p>
           <p>
-            <a href="mailto:Miko.Mattila@student.lut.fi">
-              Miko.Mattila@student.lut.fi
+            <a href="mailto:pj@ruut.me">
+              pj@ruut.me
             </a>
           </p>
           <h3>Varapuheenjohtaja</h3>
           <p>Noora Parkko</p>
           <p>
-            <a href="mailto:noora.parkko@student.lut.fi">
-              noora.parkko@student.lut.fi
+            <a href="mailto:pj@ruut.me">
+              pj@ruut.me
             </a>
           </p>
           <h3>Rahastonhoitaja</h3>
           <p>Tuomas Mustakallio</p>
           <p>
-            <a href="mailto:tuomas.mustakallio@student.lut.fi">
-              tuomas.mustakallio@student.lut.fi
+            <a href="mailto:laskutus@ruut.me">
+              laskutus@ruut.me
             </a>
           </p>
           <h3>ATK-vastaava</h3>
           <p>Konsta Keski-Mattinen</p>
           <p>
-            <a href="mailto:konsta.keski-mattinen@student.lut.fi">
-              konsta.keski-mattinen@student.lut.fi
+            <a href="mailto:atk@ruut.me">
+              atk@ruut.me
             </a>
           </p>
           <h3>Sihteeri</h3>
           <p>Leevi Laitala</p>
           <p>
-            <a href="mailto:leevi.laitala@student.lut.fi">
-              leevi.laitala@student.lut.fi
+            <a href="mailto:sihteeri@ruut.me">
+              sihteeri@ruut.me
             </a>
           </p>
           <h3>Hallituksen jäsen</h3>
           <p>Jeremias Walhsten</p>
           <p>
-            <a href="mailto:jeremias.walhsten@student.lut.fi">
-              jeremias.walhsten@student.lut.fi
+            <a href="mailto:jeremias.walhsten@ruut.me">
+              jeremias.walhsten@ruut.me
             </a>
           </p>
         </div>
